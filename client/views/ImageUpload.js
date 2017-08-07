1884a87dd70e33095496e13fa20c744ae84b9a32
dnajafi <dbnajafi@gmail.com>
2017-08-06 22:32:56 -0700

    able to upload to s3 which triggers lambda function

--- client/views/ImageUpload.js
+++ client/views/ImageUpload.js
@@ -1,28 +1,117 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, AsyncStorage, CameraRoll, ImagePickerIOS } from 'react-native';
+import { Button } from 'native-base';
+import { RNS3 } from 'react-native-aws3';
+
 
 export default class ImageUpload extends React.Component {
 
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        token: ''
-      };
-    }
+    this.state = {
+      token: '',
+      S3_BUCKET: '',
+      S3_KEY: '',
+      S3_LOCATION: '',
+      S3_SECRET: '',
+      photos: [],
+      image: null
+    };
+  }
 
   static navigationOptions = (props) => ({
     title: 'Image Upload Page'
   });
 
-  componentDidMount() {
+  onPressS3Data() {
+    return fetch('http://localhost:8080/api/gets3data?access_token='+this.state.token, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
 
+      console.log('1111111');
+      console.log(responseJson);
+
+      this.setState({ 
+        S3_BUCKET: responseJson["S3_BUCKET"],
+        S3_KEY: responseJson["S3_KEY"],
+        S3_LOCATION: responseJson["S3_LOCATION"],
+        S3_SECRET: responseJson["S3_SECRET"]
+      });
+
+    })
+    .catch((error) => {
+      console.log('2222222');
+      console.error(error);
+    });
+  }
+
+  sendToS3() {
+
+    const file = {
+      // `uri` can also be a file system path (i.e. file://)
+      uri: this.state.image,
+      name: "food.png",
+      type: "image/png"
+    }
+
+
+    const options = {
+      bucket: this.state.S3_BUCKET,
+      region: "us-west-2",
+      accessKey: this.state.S3_KEY,
+      secretKey: this.state.S3_SECRET,
+      successActionStatus: 201
+    }
+
+
+    RNS3.put(file, options).then(response => {
+      if (response.status !== 201)
+        throw new Error("Failed to upload image to S3");
+      console.log(response.body);
+      /**
+       * {
+       *   postResponse: {
+       *     bucket: "your-bucket",
+       *     etag : "9f620878e06d28774406017480a59fd4",
+       *     key: "uploads/image.png",
+       *     location: "https://your-bucket.s3.amazonaws.com/uploads%2Fimage.png"
+       *   }
+       * }
+       */
+    });
+
+
+
+
+  }
+
+  uploadPhotoS3() {
+
+    console.log('HIT UPLOAD PHOTO FUNCTION');
+
+    // openSelectDialog(config, successCallback, errorCallback);
+    ImagePickerIOS.openSelectDialog({}, imageUri => {
+      console.log('HIT THE GOOD SPOT');
+      console.log(imageUri);
+      this.setState({ image: imageUri });
+      this.sendToS3();
+    }, error => console.error(error));
+
+  }
+
+
+  componentDidMount() {
     AsyncStorage.getItem('currToken')
     .then((value) => {
       this.setState({ 'token': value });
     })
-
   }
 
 
@@ -31,6 +120,16 @@ export default class ImageUpload extends React.Component {
       <View style={styles.container}>
         <Text>Image Upload Screen</Text>
         <Text>Current token: {this.state.token}</Text>
+        <Button style={styles.button} onPress={() => this.onPressS3Data()}>
+          <Text style={styles.loginText}>Get S3 Credentials</Text>
+        </Button>
+        {this.state.S3_BUCKET ? 
+            <Button style={styles.button} onPress={() => this.uploadPhotoS3()}>
+              <Text style={styles.loginText}>Upload a Photo</Text>
+            </Button> :
+            <Text>Press button above to access S3 Credentials</Text>
+        }
+        {this.state.photos ? this.state.photos : <Text>No photos in state</Text>}
       </View>
     );
   }
--- server/server/routes/api.js
+++ server/server/routes/api.js
@@ -1,8 +1,31 @@
+const configS3 = require('../config/s3credentials');
+
+
 module.exports = (router, passport) => {
 	
 	// this router middleware protects these routes with token based authentication
 	// don't need sessions since we are using token authentication
 	// router.use(passport.authenticate('bearer', { session: false })); --> placed inside the actual route now
+
+
+	router.get('/gets3data', (req, res, next) => {
+
+		console.log('1111222222');
+
+		if(req.query.access_token) {
+			console.log('HIT HERE!!!!!!!!!!!');
+			next(); // move to passport authentication middleware
+		}
+		else
+			next('route'); // skip next middleware and go to next route ('/testAPI'); throws away remaining middlewares in this route
+
+	}, passport.authenticate('bearer', { session: false }), (req, res) => {
+
+		console.log('*********** HIT HERE *************');
+
+		res.json(configS3);
+		// res.json({ secretData: 'abc123', Authenticated: true });
+	});
 
 	router.get('/testAPI', (req, res, next) => {
 		if(req.query.access_token)
